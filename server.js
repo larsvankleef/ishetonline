9431d926cc303fd864b627ecaa699a3f88182e5e
larsvankleef <larsvankleef@outlook.com>
2019-04-17 15:41:45 +0200

    inital commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const player = require('play-sound')(opts = {})
+
+const axios = require('axios')
+
+let online = false
+
+setInterval(async () => {
+  const status = await ping()
+  if (online != status) {        
+    if (status) {
+      console.log('online')
+      player.play('./holygrenade.mp3')
+    } else {
+      console.log('offline')
+      player.play('./css1_scream1.mp3')
+    }
+  }
+
+  online = status
+}, 2000)
+
+async function ping () {
+  try {
+    await axios.get('http://stackkit.services')
+    return true
+  } catch (err) {
+    return false
+  }
+}
